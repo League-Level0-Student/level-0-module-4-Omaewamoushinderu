3767b259011c014dd2b5f7988639404acf097551
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-27 13:17:35 -0700

    TestScores

--- src/doubles_and_booleans/TestScores.java
+++ src/doubles_and_booleans/TestScores.java
@@ -1,7 +1,16 @@
 package doubles_and_booleans;
 
+import javax.swing.JOptionPane;
+
 public class TestScores {
 public static void main(String[] args) {
-	
+	String score = JOptionPane.showInputDialog("What was your test score");
+	int scores = Integer.parseInt(score);
+	if(scores > 50) {
+		JOptionPane.showMessageDialog(null, "Good job you pass");
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "You failed");
+	}
 }
 }
