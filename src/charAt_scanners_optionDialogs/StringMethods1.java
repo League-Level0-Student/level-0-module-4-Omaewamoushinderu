1571db8e795e2d88a47fa0385f523b3a77379eb2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-11 13:22:10 -0700

    HappyPet(IP), PiAloud, SleepyHead, StringMethods1

--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -8,16 +8,22 @@ public class StringMethods1 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-String poo = 
+String poo = "ooga booga";
+
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		System.out.println("charAT");
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
-		// 4. Using a for loop, print one char at time to the console.
+System.out.println(poo.length());
+char e = ('o');		
+// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+for(int i=0 ; i<poo.length() ; i++) {
+	if(e==poo.charAt(i)) {
+		System.out.println(e + " is at index " + i);
+	}
+}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
--- src/doubles_and_booleans/SleepyHead.java
+++ src/doubles_and_booleans/SleepyHead.java
@@ -11,18 +11,18 @@ public class SleepyHead {
     public static void main(String[] args) {
         
         boolean isWeekday, isVacation;
-C        
+        
         /* You MUST use the above boolean variables in your code */
         
         /*
          * Ask the user for these values using a confirm dialog like the one below
          * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
          */
-    
-
-
-        /*
-         * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
+JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+System.out.println("sleep in");
+System.out.println("get up lazey bones");
+System.out.println("sleep in");/*
+         *Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
          * print “get up lazybones!�? If it is a weekday, and we are on vacation,
          * print “sleep in�?.
          */
