4df37c4346a7026cb04889376e6e2686598306a4
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-01 12:58:26 -0700

    ChooseYourOwnAdventure

--- src/charAt_scanners_optionDialogs/ChooseYourOwnAdventure.java
+++ src/charAt_scanners_optionDialogs/ChooseYourOwnAdventure.java
@@ -1,7 +1,10 @@
 package charAt_scanners_optionDialogs;
 
+import javax.swing.JOptionPane;
+
 public class ChooseYourOwnAdventure {
 public static void main(String[] args) {
-	
+	int task = JOptionPane.showOptionDialog(null, "Hello adventurer what do you wanna do today?", "What to do?", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			new String[] { "Food", "Water", "Pet" }, null);
 }
 }
